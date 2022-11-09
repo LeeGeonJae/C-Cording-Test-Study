19e70156c18f12292038bcf68e5189dc387e62f9
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2022-11-10 02:49:10 +0900

    atoi(s.c_str())

--- main.cpp
+++ main.cpp
@@ -4,53 +4,11 @@
 
 using namespace std;
 
-vector<string> split(string input, string delimiter)
+int main() 
 {
-	vector<string> ret;
-	long long pos = 0;
-	string token = "";
-	while ((pos = input.find(delimiter)) != string::npos)
-	{
-		token = input.substr(0, pos);
-		ret.push_back(token);
-		input.erase(0, pos + delimiter.length());
-	}
-	ret.push_back(input);
-	return ret;
-}
-
-vector<string> split_debug(string input, string delimiter)
-{
-	vector<string> ret;
-	long long pos = 0;
-	string token = "";
-	while ((pos = input.find(delimiter)) != string::npos)
-	{
-		long long pos = input.find(delimiter);
-		cout << "POS : " << pos << endl;
-		if (pos == string::npos)
-			break;
-		token = input.substr(0, pos);
-		ret.push_back(token);
-		input.erase(0, pos + delimiter.length());
-	}
-	ret.push_back(input);
-	return ret;
-}
-
-int main()
-{
-	string s = "안녕하세요. 큰돌이는 킹갓제너럴 천재입니다. 정말이에요!";
-	string d = " ";
-	vector<string> a = split(s, d);
-	string s2 = "aaaa bbb ccc ee ddddddddd!";
-	vector<string> c = split_debug(s2, d);
-	// 4 3 5 2 띄어쓰기를 찾아 해당 첫번째 인덱스를 반환한다.
-	// 문자열이 erase되기 때문에 aaaa, bb 이런식으로 되는 것도 생각.
-	for (string b : a)
-	{
-		cout << b << endl;
-	}
-
+	string s = "1";
+	string s2 = "amumu";
+	cout << atoi(s.c_str()) << endl;
+	cout << atoi(s2.c_str()) << endl;
 	return 0;
 }
