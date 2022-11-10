4402bf82715a934c0195f10032e7d6ea35f1a74c
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2022-11-10 22:53:20 +0900

    vector

--- main.cpp
+++ main.cpp
@@ -1,21 +1,66 @@
 #include <iostream>
 #include <stdio.h>
 #include <string>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
+vector<int> v;
+
 int main()
 {
-	double ret = 2.12345;
-	int n = 2;
-	int a = (int)round(ret / double(n));
+	for (int i = 1; i <= 10; i++)
+	{
+		// vector.push_back(i) 뒤부터 i요소를 더하겠다.
+		v.push_back(i);
+	}
 
-	cout << a << "\n";
+	for (int a : v)
+	{
+		cout << a << " ";
+	}
+	cout << endl;
 
-	cout << round(ret) << endl;
-	cout << (int)ret << endl;
-	cout << (double)n << endl;
-	cout << double(n) << endl;
+	// vector.pop_back(); 맨 뒤부터 지우는 함수
+	v.pop_back();
+	for (int a : v)
+	{
+		cout << a << " ";
+	}
+	cout << endl;
+
+	// vector.erase(처음 자리 수, 마지막 자리 수)
+	v.erase(v.begin(), v.begin() + 1);
+	for (int a : v)
+	{
+		cout << a << " ";
+	}
+	cout << endl;
+
+	// find(처음 자리 수, 마지막 자리 수, ?)
+	auto a = find(v.begin(), v.end(), 100);
+	if (a == v.end())
+	{
+		cout << "not found" << endl;
+	}
+
+	// fill (처음 자리 수, 마지막 자리 수, 채울 값)
+	fill(v.begin(), v.end(), 10);
+	for (int a : v)
+	{
+		cout << a << " ";
+	}
+	cout << endl;
+
+	// vector.clear()함수 
+	v.clear();
+	cout << "아무것도 없을까?" << endl;
+	for (int a : v)
+	{
+		cout << a << " ";
+	}
+	cout << endl;
 
 	return 0;
 }
