6f90bb1dcb25dd53892c1da43b2fd7f4a83d408c
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2022-11-10 20:39:31 +0900

    getline

--- main.cpp
+++ main.cpp
@@ -4,25 +4,19 @@
 
 using namespace std;
 
-char a[10][10];
+int T;
+string s;
 
 int main()
 {
-	for (int i = 0; i < 2; i++)
+	cin >> T;
+	//string bufferflush;
+	//getline(cin, bufferflush);
+	
+	for (int i = 0; i < T; i++)
 	{
-		for (int j = 0; j < 4; j++)
-		{
-			scanf_s(" %c", &a[i][j]);
-		}
-	}
-
-	for (int i = 0; i < 2; i++)
-	{
-		for (int j = 0; j < 4; j++)
-		{
-			cout << a[i][j];
-		}
-		cout << endl;
+		getline(cin, s);
+		cout << s << endl;
 	}
 
 	return 0;
