d734821e4f04c0d62c44297619166fdcc83cb128
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2022-11-10 18:49:17 +0900

    따닥따닥 붙어있는 것을 입력받는 법 ( string )

--- main.cpp
+++ main.cpp
@@ -4,16 +4,28 @@
 
 using namespace std;
 
+int n, m, a[10][10];
+string s;
+
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> s;
+		for (int j = 0; j < m; j++)
+		{
+			a[i][j] = s[j] - '0';
+		}
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			cout << a[i][j];
+		}
+		cout << endl;
+	}
 
-	int a = -1;
-	cout << bool(a) << endl;
-	a = 0;
-	cout << bool(a) << endl;
-	a = 3;
-	cout << bool(a) << endl;
+	return 0;
 }
