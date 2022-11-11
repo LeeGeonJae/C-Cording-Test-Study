cb03b7f97e8b7f057ff98079a88f7e5a079c592d
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2022-11-11 22:19:36 +0900

    vector<pair<int, int>> v; 를 sort로 오름차순 정렬하기

--- main.cpp
+++ main.cpp
@@ -7,42 +7,17 @@
 
 using namespace std;
 
-vector<int> a;
-int b[5];
+vector<pair<int, int>> v;
 
 int main()
 {
-	for (int i = 5; i >= 1; i--)
+	for (int i = 10; i >= 1; i--)
 	{
-		b[i - 1] = i;
+		v.push_back({ i, 10 - i });
 	}
-	for (int i = 5; i >= 1; i--)
-	{
-		a.push_back(i);
-	}
+	sort(v.begin(), v.end());
 
-	// 오름차순
-	sort(b, b + 5);
-	sort(a.begin(), a.end());
-	for (int i : b) cout << i << ' ';
-	cout << endl;
-	for (int i : a) cout << i << ' ';
-	cout << endl;
-
-	sort(b, b + 5, less<int>());
-	sort(a.begin(), a.end(), less<int>());
-	for (int i : b) cout << i << ' ';
-	cout << endl;
-	for (int i : a) cout << i << ' ';
-	cout << endl;
-
-	// 내림차순
-	sort(b, b + 5, greater<int>());
-	sort(a.begin(), a.end(), greater<int>());
-	for (int i : b) cout << i << ' ';
-	cout << endl;
-	for (int i : a) cout << i << ' ';
-	cout << endl;
+	for (auto it : v) cout << it.first << " : " << it.second << endl;
 	
 	return 0;
 }
