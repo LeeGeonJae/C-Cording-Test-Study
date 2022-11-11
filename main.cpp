87e8bb982d9715905a492c51b203c8890ec4c36d
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2022-11-12 05:18:33 +0900

    struct를 정렬할 때 sort 커스텀 오퍼레이터를 써서 정렬하는 방법

--- main.cpp
+++ main.cpp
@@ -9,14 +9,12 @@ using namespace std;
 struct Point
 {
 	int y, x;
-	Point(int y, int x) : y(y), x(x) {}
-	Point() { y = -1; x = -1; }
-	bool operator < (const Point& a) const
-	{
-		if (x == a.x) return y < a.y;
-		return x < a.x;
-	}
 };
+bool cmp(const Point& a, const Point& b)
+{
+	cout << a.x << "a " << b.x << "b " << endl;
+	return a.x > b.x;
+}
 
 int main()
 {
@@ -24,10 +22,9 @@ int main()
 
 	for (int i = 10; i >= 1; i--)
 	{
-		Point a = { i, i };
-		v.push_back(a);
+		v.push_back({ i, 10 - i });
 	}
-	sort(v.begin(), v.end());
+	 sort(v.begin(), v.end(), cmp);
 	for (auto it : v) cout << it.y << " : " << it.x << endl;
 
 	return 0;
