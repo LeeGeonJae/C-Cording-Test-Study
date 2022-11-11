56406806168d0fe90c50212cf0fe43f01d77c6ce
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2022-11-11 21:28:47 +0900

    2차원 배열

--- main.cpp
+++ main.cpp
@@ -6,22 +6,40 @@
 
 using namespace std;
 
-int a[5];
-int temp[5];
+const int mxy = 3;
+const int mxx = 4;
+
+int a[mxy][mxx];
 
 int main()
 {
-	for (int i = 0; i < 5; i++)
+	for (int i = 0; i < mxy; i++)
 	{
-		a[i] = i;
+		for (int j = 0; j < mxx; j++)
+		{
+			a[i][j] = (i + j);
+		}
 	}
 	
-	memcpy(temp, a, sizeof(a));
+	// good
+	for (int i = 0; i < mxy; i++)
+	{
+		for (int j = 0; j < mxx; j++)
+		{
+			cout << a[i][j] << ' ';
+		}
+		cout << endl;
+	}
 
-	for (int i : temp)
+	// bad
+	for (int i = 0; i < mxx; i++)
 	{
-		cout << i << " ";
+		for (int j = 0; j < mxy; j++)
+		{
+			cout << a[j][i] << ' ';
+		}
+		cout << endl;
 	}
-	
+
 	return 0;
 }
