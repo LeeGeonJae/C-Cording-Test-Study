3234175cabaac78b8d04eaf8142ad8cacd26a3c5
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2022-11-12 06:56:51 +0900

    포인터

--- main.cpp
+++ main.cpp
@@ -2,15 +2,22 @@
 #include <stdio.h>
 #include <string>
 #include <vector>
+#include <map>
+#include <unordered_map>
 
 using namespace std;
 
 int main()
 {
-	string a = "abcda";
-	string *b = &a;
-	cout << b << endl;
-	cout << *b << endl;
+	vector<int> v;
+
+	for (int i = 1; i <= 5; i++) v.push_back(i);
+	for (int i = 0; i < 5; i++)
+	{
+		cout << i << "번째 요소 : " << *(v.begin() + i) << endl;
+		cout << &*(v.begin() + i) << endl;
+	}
+	// cout << v.begin() << endl; // 에러
 
 	return 0;
 }
