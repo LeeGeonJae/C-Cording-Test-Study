cef8a301bd086286b825c8a83a3f694edbffba36
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2022-11-12 01:06:10 +0900

    multiset

--- main.cpp
+++ main.cpp
@@ -8,21 +8,35 @@ using namespace std;
 
 int main()
 {
-	set<pair<string, int>> st;
-	st.insert({ "test", 1 });
-	st.insert({ "test", 1 });
-	st.insert({ "test", 1 });
-	st.insert({ "test", 1 });
-	cout << st.size() << " : st»çÀÌÁî" << endl;
+	multiset<int> s;
+	s.insert(12);
+	s.insert(10);
+	s.insert(2);
+	s.insert(10);
+	s.insert(90);
+	s.insert(85);
+	s.insert(45);
 
-	set<int> st2;
-	st2.insert(1);
-	st2.insert(2);
-	st2.insert(2);
-	for (auto it : st2)
-	{
-		cout << it << endl;
-	}
+	cout << "Multiset elements after sort" << endl;
+	for (auto it = s.begin(); it != s.end(); it++)
+		cout << *it << ' ';
+	cout << endl;
+
+	auto it1 = s.find(10);
+	auto it2 = s.find(90);
+
+	// elements from 10 to elements befor 90
+	// erased
+	s.erase(it1, it2);
+
+	cout << "Multiset Elements after erase" << endl;
+
+	for (auto it = s.begin(); it != s.end(); it++)
+		cout << (*it) << ' ';
+	s.erase(s.begin());
+	cout << endl;
+	for (auto it = s.begin(); it != s.end(); it++)
+		cout << (*it) << ' ';
 
 	return 0;
 }
