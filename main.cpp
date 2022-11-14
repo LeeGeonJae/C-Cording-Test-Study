9a97c6649d1dc7a16c3254776fd90bf18a5ee5e0
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2022-11-14 20:21:32 +0900

    순열

--- main.cpp
+++ main.cpp
@@ -2,28 +2,26 @@
 #include <stdio.h>
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <cstdio>
 
 using namespace std;
 
-//어떤 정수들이 있습니다.
-//이 정수들의 절댓값을 차례대로 담은 정수 배열 absolutes와 이 정수들의 부호를 차례대로 담은 불리언 배열 signs가 매개변수로 주어집니다.
-//실제 정수들의 합을 구하여 return 하도록 solution 함수를 완성해주세요.
-
-int solution(vector<int> absolutes, vector<bool> signs) {
-	int answer = 0;
-	int j = 0;
-
-	for (int i : absolutes)
+void printV(vector<int>& v)
+{
+	for (int i = 0; i < v.size(); i++)
 	{
-		if (*(signs.begin() + j))
-		{
-			answer = answer + i;
-		}
-		else
-		{
-			answer = answer - i;
-		}
-		j = j + 1;
+		cout << v[i] << " ";
 	}
-	return answer;
+	cout << endl;
+}
+
+int main()
+{
+	int a[3] = { 1, 2, 3 };
+	vector<int> v;
+
+	for (int i = 0; i < 3; i++) v.push_back(a[i]);
+
+	return 0;
 }
