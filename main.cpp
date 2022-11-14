4ec13847cf73d898021b491ba6b6c7bda84b2405
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2022-11-15 07:47:29 +0900

    순열 ( 재귀함수 )

--- main.cpp
+++ main.cpp
@@ -7,6 +7,9 @@
 
 using namespace std;
 
+int a[3] = { 1, 2, 3 };
+vector<int> v;
+
 void printV(vector<int>& v)
 {
 	for (int i = 0; i < v.size(); i++)
@@ -15,13 +18,25 @@ void printV(vector<int>& v)
 	}
 	cout << endl;
 }
+void makePermutation(int n, int r, int depth)
+{
+	if (r == depth)
+	{
+		printV(v);
+		return;
+	}
+	for (int i = depth; i < n; i++)
+	{
+		swap(v[i], v[depth]);
+		makePermutation(n, r, depth + 1);
+		swap(v[i], v[depth]);
+	}
+	return;
+}
 
 int main()
 {
-	int a[3] = { 1, 2, 3 };
-	vector<int> v;
-
 	for (int i = 0; i < 3; i++) v.push_back(a[i]);
-
+	makePermutation(3, 3, 0);
 	return 0;
 }
