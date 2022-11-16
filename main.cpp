d4a01404973dd351b97ce300e55e2b7bff71c60f
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2022-11-16 20:33:04 +0900

    조합 ( for... i, j, k )

--- main.cpp
+++ main.cpp
@@ -9,32 +9,18 @@ using namespace std;
 
 int n = 5, k = 3, a[5] = { 1,2,3,4,5 };
 
-void print(vector<int> b)
-{
-	for (int i : b) cout << i << " ";
-	cout << '\n';
-}
-
-void combi(int start, vector<int>b)
-{
-	if (b.size() == k)
-	{
-		print(b);
-		return;
-	}
-	for (int i = start + 1; i < n; i++)
-	{
-		b.push_back(i);
-		combi(i, b);
-		b.pop_back();
-	}
-	return;
-}
-
 int main()
 {
-	vector<int> b;
-	combi(-1, b)
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			for (int k = 0; k < j; k++)
+			{
+				cout << i << " " << j << " " << k << '\n';
+			}
+		}
+	}
 
 	return 0;
 }
