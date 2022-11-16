1b8b378438475c98679493e4e592276243a45b04
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2022-11-16 20:28:56 +0900

    조합 ( 재귀함수 )

--- main.cpp
+++ main.cpp
@@ -7,26 +7,34 @@
 
 using namespace std;
 
-int n = 5, k = 3, a[5] = { 1, 2, 3, 4, 5 };
+int n = 5, k = 3, a[5] = { 1,2,3,4,5 };
 
-void print(vector<int> b) {
-	for (int i : b)cout << i << " ";
+void print(vector<int> b)
+{
+	for (int i : b) cout << i << " ";
 	cout << '\n';
 }
-void combi(int start, vector<int> b) {
-	if (b.size() == k) {
+
+void combi(int start, vector<int>b)
+{
+	if (b.size() == k)
+	{
 		print(b);
 		return;
 	}
-	for (int i = start + 1; i < n; i++) {
+	for (int i = start + 1; i < n; i++)
+	{
 		b.push_back(i);
 		combi(i, b);
 		b.pop_back();
 	}
 	return;
 }
-int main() {
+
+int main()
+{
 	vector<int> b;
-	combi(-1, b);
+	combi(-1, b)
+
 	return 0;
 }
