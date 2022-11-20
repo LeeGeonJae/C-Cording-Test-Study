b068c87e6742b34bc17766c445f29f70291ae79e
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2022-11-20 22:02:43 +0900

    upper_bound, lower_bound

--- main.cpp
+++ main.cpp
@@ -9,10 +9,16 @@ using namespace std;
 
 int main()
 {
-	vector<int> a;
-	for (int i = 1; i <= 3; i++) a.push_back(i);
-	for (int i = 5; i <= 10; i++) a.push_back(i);
-	cout << lower_bound(a.begin(), a.end(), 4) - a.begin() << '\n';
+	vector<int> v;
+	for (int i = 2; i <= 5; i++)v.push_back(i);
+	v.push_back(7);
+	// 2 3 4 5 7
+	cout << upper_bound(v.begin(), v.end(), 6) - v.begin() << "\n";
+	cout << lower_bound(v.begin(), v.end(), 6) - v.begin() << "\n";
+	cout << upper_bound(v.begin(), v.end(), 9) - v.begin() << "\n";
+	cout << lower_bound(v.begin(), v.end(), 9) - v.begin() << "\n";
+	cout << upper_bound(v.begin(), v.end(), 0) - v.begin() << "\n";
+	cout << lower_bound(v.begin(), v.end(), 0) - v.begin() << "\n";
 
 	return 0;
 }
