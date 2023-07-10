4c41c109e0b5aaab23b2e81f74405c826716ec9c
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2023-07-10 22:51:43 +0900

    백준 17387번 문제 : 선분 교차2

--- main.cpp
+++ main.cpp
@@ -1,22 +1,69 @@
 #include <iostream>
-#include <stdio.h>
-#include <string>
 #include <vector>
-#include <algorithm>
-#include <cstdio>
 
 using namespace std;
 
-vector<int> v;
-
-int main() 
+struct Point
 {
-	for (int i = 1; i < 10; ++i) v.push_back(i); // 1 2 3 4 5 6 7 8 9
-	//rotate(v.begin(),v.begin()+1, v.end());
-	// 2 3 4 5 6 7 8 9 1 앞으로 할 땐 이렇게
-	rotate(v.begin(), v.begin() + v.size() - 1, v.end());
-	// 9 1 2 3 4 5 6 7 8 뒤로 갈 땐 이렇게
-	for (std::vector<int>::iterator it = v.begin(); it != v.end(); ++it)
-		std::cout << ' ' << *it;
-	std::cout << '\n';
+	long long x, y;
+
+	bool operator<=(const Point& _p)
+	{
+		if (x == _p.x)
+			return y <= _p.y;
+		return x <= _p.x;
+	}
+};
+
+struct Line
+{
+	Point p1, p2;
+};
+
+int CCW(const Point& _p1, const Point& _p2, const Point& _p3)
+{
+	long long x1 = (_p2.x - _p1.x);
+	long long y1 = (_p2.y - _p1.y);
+	long long x2 = (_p3.x - _p1.x);
+	long long y2 = (_p3.y - _p1.y);
+
+	long long res = x1 * y2 - x2 * y1;
+
+	if (res > 0) return 1;			// 반시계 방향
+	else if (res < 0) return -1;	// 시계 방향
+	else return 0;					// 일직선
+}
+
+int main()
+{
+	Line l1, l2;
+	bool Check = false;
+
+	cin >> l1.p1.x >> l1.p1.y >> l1.p2.x >> l1.p2.y >> l2.p1.x >> l2.p1.y >> l2.p2.x >> l2.p2.y;
+
+	int std1 = CCW(l1.p1, l1.p2, l2.p1) * CCW(l1.p1, l1.p2, l2.p2);
+	int std2 = CCW(l2.p1, l2.p2, l1.p1) * CCW(l2.p1, l2.p2, l1.p2);
+
+	if (std1 <= 0 && std2 <= 0)
+	{
+		if (std1 == 0 && std2 == 0)
+		{
+			if (l1.p2 <= l1.p1) swap(l1.p1, l1.p2);
+			if (l2.p2 <= l2.p1) swap(l2.p1, l2.p2);
+
+			if (l1.p1 <= l2.p2 && l2.p1 <= l1.p2)
+				Check = true;
+			else
+				Check = false;
+		}
+		else Check = true;
+	}
+	else Check = false;
+
+	if (Check)
+		cout << 1 << endl;
+	else
+		cout << 0 << endl;
+
+	return 0;
 }
